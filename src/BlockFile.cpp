cbd776ed6218fc8e6a6cc92173d33ac89581cf82
Luv Khemani(RyzLinux) <luvb@hotmail.com>
2018-12-24 12:47:36 +0800

    -Added templated readMem -Added more code to read further into block

--- src/BlockFile.cpp
+++ src/BlockFile.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <iomanip>
 #include "BlockFile.h"
 
@@ -15,24 +14,65 @@ BlockFile::~BlockFile()
 
 void BlockFile::readBlockFile(const std::string& filename)
 {
-    std::ifstream fi;
-    unsigned int size = 0;
+    unsigned int INT = 0;
     fi.open(filename, std::ios::binary);
-    fi.read(reinterpret_cast<char*>(&size), sizeof(size));  //read magic bytes
-    std::cout<<std::hex<<size<<std::endl;
-    fi.read(reinterpret_cast<char*>(&size), sizeof(size));  //read size
-    std::cout<<std::dec<<size<<std::endl;
-    fi.read(reinterpret_cast<char*>(&size), sizeof(size));  //read version
-    std::cout<<size<<std::endl;
+    //read magic bytes
+    readMem(INT);
+    std::cout<<std::hex<<INT<<std::endl;
+    //read size
+    readMem(INT);
+    std::cout<<std::hex<<INT<<std::endl;
+    //read version
+    readMem(INT);
+    std::cout<<INT<<std::endl;
 
+    //read prevHash
     unsigned char prevH[32];
-    fi.read(reinterpret_cast<char*>(prevH), sizeof(prevH));  //read prevHash
-    fi.read(reinterpret_cast<char*>(prevH), sizeof(prevH));  //read merkleRoot
+    readMem(prevH);
     std::cout<<std::hex;
     for(int i = 0; i < 32; ++i)
     {
         std::cout<<static_cast<int>(prevH[i]);
     }
+    //read merkleRoot
+    readMem(prevH);
+    std::cout<<std::endl<<std::hex;
+    for(int i = 0; i < 32; ++i)
+    {
+        std::cout<<static_cast<int>(prevH[i]);
+    }
+    readMem(INT);  //read timestamp
+    std::cout<<std::endl<<std::dec<<INT<<std::endl;
+    readMem(INT);  //read target difficulty
+    std::cout<<INT<<std::endl;
+    readMem(INT);  //read nonce
+    std::cout<<INT<<std::endl;
 
+    char num = 0xfd;
+    readMem(num);  //TODO: Change to CompactSize
+    std::cout<<static_cast<int>(num)<<" "<<std::hex<<static_cast<int>(num)<<std::endl;
+    if(num > 0xfd)
+    {
+        std::cout<<"Not implemented yet, cant read block";
+    }
+    else
+    {
+        readMem(INT);  //read version
+        std::cout<<std::endl<<std::dec<<INT<<std::endl;
+        char numInputs = 0;
+        readMem(numInputs); //read numTxns //TODO: Change to CompactSize
+        std::cout<<static_cast<int>(numInputs)<<" "<<std::hex<<static_cast<int>(numInputs)<<std::endl;
+
+        readMem(prevH);  //read inputHash
+        std::cout<<std::hex;
+        for(int i = 0; i < 32; ++i)
+        {
+            std::cout<<static_cast<int>(prevH[i]);
+        }
+        readMem(INT);  //readIndex
+        std::cout<<std::endl<<INT<<std::endl;
+        readMem(num); //readScriptLength //TODO: Change to CompactSize
+        std::cout<<std::dec<<static_cast<int>(num)<<" "<<std::hex<<static_cast<int>(num)<<std::endl;
+    }
     fi.close();
 }
--- src/BlockFile.h
+++ src/BlockFile.h
@@ -1,6 +1,7 @@
 #ifndef BLOCKFILE_H
 #define BLOCKFILE_H
 #include <string>
+#include <fstream>
 
 class BlockFile
 {
@@ -8,10 +9,16 @@ class BlockFile
         BlockFile();
         virtual ~BlockFile();
         void readBlockFile(const std::string& filename);
-
+        void readCompactSize(unsigned int& a);
+        template<typename T>
+        inline void readMem(T& a)
+        {
+            fi.read(reinterpret_cast<char*>(&a), sizeof(a));  //read numTxns
+        }
     protected:
 
     private:
+        std::ifstream fi;
 };
 
 #endif // BLOCKFILE_H
